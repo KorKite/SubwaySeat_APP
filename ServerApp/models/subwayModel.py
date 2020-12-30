64f8d0d6a9ee01dfe7f8997278466b47293fee29
고준서 <sta06167@naver.com>
2020-12-30 19:32:53 +0900

    status표시 업데이트
    위치 표시 할 때, 데이터베이스에 한글로 표시되도록 변경

--- ServerApp/models/subwayModel.py
+++ ServerApp/models/subwayModel.py
@@ -38,12 +38,14 @@ class subwayModel:
         line = locationData["subwayNm"] # 호선
         station = locationData["statnNm"] #현재 위치하고 있는 역
         trainStatus = locationData["trainSttus"] #열차 출발 여부
+        statusList = ["진입", "도착", "출발"]
         finalDestination = locationData["statnTnm"] # 종착역
         updown = locationData["updnLine"] #상행 하행 여부 (0 상행, 1 하행)
         updownList = ["상행", "하행"]
         updatedData = {
             "현재역":station,
-            "열차출발여부":trainStatus,
+            "열차출발여부":statusList[int(trainStatus)],
             "종착역": finalDestination
         }
+        print(updatedData)
         self.db.child("SubwayLocation").child(line).child(updownList[int(updown)]).child(trainId).update(updatedData)
